2003b572b88aaa5a85d53b86eba0b031f9bb3888
Jose Saad <josemiguelsaad13@gmail.com>
2021-10-08 14:00:05 -0400

    errorsito arreglado

--- src/components/Product.js
+++ src/components/Product.js
@@ -27,8 +27,8 @@ const useStyles = makeStyles((theme) => ({
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
+    transition: ('transform', {
+      duration: '.5s',
     }),
   },
   expandOpen: {
