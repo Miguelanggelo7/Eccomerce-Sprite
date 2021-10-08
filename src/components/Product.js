948c3840272850b707c9c2acf7adc195ce02fd43
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2021-10-08 13:33:12 -0400

    First commit

---
+++ src/components/Product.js
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { AddShoppingCart } from '@mui/icons-material';
+import { maxWidth } from '@mui/system';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  action: {
+    marginTop: '1rem',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+}))
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Product() {
+  const [expanded, setExpanded] = React.useState(false);
+  const classes = useStyles();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        action={
+          <Typography
+            variant='h5'
+            color='GrayText'
+            className={classes.action}
+          >
+            {50}
+          </Typography>
+        }
+        title="Shoes"
+        subheader="in Stock"
+      />
+      <CardMedia
+        className={classes.media}
+        component="img"
+        height="194"
+        image="https://dm3.com/wp-content/uploads/2017/09/zapatos-nike-795x400.png"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Running Shoes
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
+        {Array(4)
+          .fill()
+          .map((_, i) => (
+            <p>&#11088;</p>
+          ))
+        }
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Shoes for running
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
---
+++ src/product-data.js
@@ -0,0 +1,13 @@
+const products = [
+
+  {
+    id: 1,
+    name: "Shoes",
+    productType: "Nike Shoes",
+    price: 50,
+    rating: 4,
+    image: "https://dm3.com/wp-content/uploads/2017/09/zapatos-nike-795x400.png",
+    description: "Shoes for running"
+  }
+
+]
